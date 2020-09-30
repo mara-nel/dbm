96fbb26b3d7fc1817b74cce04b74709ec53609b3
wynnelson <wesslnelson@gmail.com>
2020-09-30 18:42:55 -0400

    Added missionsRank array and new missions

--- missions.js
+++ missions.js
@@ -12,12 +12,21 @@
 //  and the second is if finishing with an all clear is possible (0 is no, 1 is yes)
 var missions = [
   [[0,0,0,0,0],[5,1]],
+  [[5,5,5,5],[3,1]],
   [[6,3,3,3,3,3,3],[6,0]],
   [[2,1,2,1,2,1,2,1,2,1],[9,1]],
   [[5,4,6,4,4,6,4,6,5,1],[9,1]],
   [[1,0,2,3],[3,1]],
   [[5,1,5,4],[3,1]],
+  [[5,5,2,5,5,3],[5,1]],
+  [[3,0,3,6,5,2,6,2],[7,1]],
   [[0,5,2,3],[3,1]],
+  [[0,5,3,1,2],[4,1]],
   [[6,5,4,3,3,3],[5,0]]
 
 ]
+
+
+// a list to keep track of mission rankings
+var missionsRecord = Array.from('F'.repeat(missions.length));
+
