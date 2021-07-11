92dfef6c4594c8a6ffa110e61d9ffab70a46e756
wynnelson <wesslnelson@gmail.com>
2021-07-11 12:17:49 -0400

    Began work to implement PWA features

---
+++ sw.js
@@ -0,0 +1,54 @@
+self.addEventListener('install', function(event) {
+  var CACHE_NAME = 'my-site-cache-v1';
+  var urlsToCache = [
+    '/comboMambo.html',
+    '/comboMambo.css',
+    '/comboMambo.js',
+    '/bricks.js',
+    '/missions.js',
+  ];
+
+  self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
